41babda10334894c256aa5d3abe871e714845402
suryaveeramreddy <84376402+suryaveeramreddy@users.noreply.github.com>
2023-03-26 21:44:18 +0530

    Add files via upload

---
+++ DOM manipulation assignment--26-03-23/DOM/script.js
@@ -0,0 +1,46 @@
+function paraOver(){
+    document.getElementById('p').style.color='red';
+}
+function paraOut(){
+    document.getElementById('p').style.color='black';
+}
+function btnOver(){
+    document.getElementById('btn').innerHTML='Click Now';
+}
+function btnOut(){
+    document.getElementById('btn').innerHTML='Here !!';
+}
+function para(){
+    let t=document.getElementById('txt').value;
+    document.getElementById('p').innerHTML=t;
+}
+function listOver1(){
+    let d=document.getElementById('i1');
+    d.style.color='blue';
+    d.style.fontSize='20px';
+}
+function listOut1(){
+    let d=document.getElementById('i1');
+    d.style.color='black';
+    d.style.fontSize='15px';
+}
+function listOver2(){
+    let d=document.getElementById('i2');
+    d.style.color='blue';
+    d.style.fontSize='20px';
+}
+function listOut2(){
+    let d=document.getElementById('i2');
+    d.style.color='black';
+    d.style.fontSize='15px';
+}
+function listOver3(){
+    let d=document.getElementById('i3');
+    d.style.color='blue';
+    d.style.fontSize='20px';
+}
+function listOut3(){
+    let d=document.getElementById('i3');
+    d.style.color='black';
+    d.style.fontSize='15px';
+}
